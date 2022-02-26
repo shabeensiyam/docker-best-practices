334993b7a638d1c5fa8696fd18d3dec3d87d6c30
root <root@Shabeen-PC.localdomain>
2022-02-27 00:03:20 +0530

    Initial Commit

---
+++ build_cache/app.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+    "time"
+    "os/user"
+    "os"
+)
+
+var logFile *os.File
+
+func main () {
+    var err error
+
+    user, err := user.Current()
+    if err != nil {
+        panic(err)
+    }
+
+    logFile, err = os.OpenFile("access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        panic(err)
+    } else {
+            fmt.Println("access.log created successfully!")
+    }
+    defer logFile.Close()
+
+    for {
+        fmt.Println("user: " + user.Username + " id: " + user.Uid)
+        time.Sleep(1 * time.Second)
+    }
+}
---
+++ lint_and_scan/policy/base.rego
@@ -0,0 +1,13 @@
+package main
+
+denylist = [
+  "ubuntu"
+]
+
+deny[msg] {
+  input[i].Cmd == "from"
+  val := input[i].Value
+  contains(val[i], denylist[_])
+
+  msg = sprintf("unallowed image found %s", [val])
+}
